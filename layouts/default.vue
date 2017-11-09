b8cd0846242d428c188cc8c9882079299cca7762
Don <772528797@qq.com>
2017-11-09 18:19:14 +0800

    开发返回顶部功能

--- layouts/default.vue
+++ layouts/default.vue
@@ -10,7 +10,7 @@
     <don-footer/>    
     <div class="top-bar">
       <div class="container">
-        <span class="to-top">
+        <span class="to-top" @click="goToTop" v-show="isShowToTop">
           <svg class="icon" aria-hidden="true">
             <use xlink:href="#icon-toTop"></use>
           </svg>
@@ -30,6 +30,57 @@ export default {
     'don-header': Header,
     'don-menu': Menu,
     'don-footer': Footer
+  },
+  data () {
+    return {
+      $docElement: null,
+      $body: null,
+      isShowToTop: false,
+      timer: null
+    };
+  },
+  mounted: function () {
+    this.$docElement = document.documentElement;
+    this.$body = document.body;
+    this.pageToTop();
+    window.addEventListener('scroll', this.debounce(this.pageToTop));
+  },
+  methods: {
+    pageToTop: function () {
+      let scrollTop = this.$body.scrollTop + this.$docElement.scrollTop;
+      if (scrollTop > 200) {
+        this.isShowToTop = true;
+      } else {
+        this.isShowToTop = false;
+      }
+      if (scrollTop === 0) {
+        clearInterval(this.timer);
+      }
+    },
+    goToTop: function () {
+      this.timer = setInterval(() => {
+        let scrollTop = this.$body.scrollTop + this.$docElement.scrollTop;
+        let speed = Math.floor(scrollTop / 6);
+        this.$body.scrollTop = this.$docElement.scrollTop = scrollTop - (speed < 1 ? 1 : speed);
+      }, 30);
+    },
+    debounce: function (fn, delay, timeout) {
+      var timer = null;
+      var last = new Date().getTime();
+      delay = delay || 300;
+      timeout = timeout || 300;
+      return () => {
+        if (timer) {
+          clearTimeout(timer);
+        }
+        timer = setTimeout(fn, delay);
+        if (new Date().getTime() > last + timeout) {
+          fn.apply(this, [].slice.call(Array, arguments));
+          last = new Date().getTime();
+          clearTimeout(timer);
+        }
+      };
+    }
   }
 };
 </script>
@@ -62,6 +113,9 @@ export default {
     .icon {
       width: 3em;
       height: 3em;
+    }
+    &:hover {
+      color: $base-color;
     }
   }
 }
